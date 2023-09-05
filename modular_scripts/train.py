0f59a942f40c00ef1bc1b1a218c3fa0d40e75334
Juan <jotadebeese@gmail.com>
2023-09-05 11:20:45 +1000

    Added train.py

---
+++ modular_scripts/train.py
@@ -0,0 +1,70 @@
+"""
+Trains a PyTorch image classification model using device-agnostic code.
+"""
+
+import os
+import torch
+import data_setup, engine, model_builder, utils
+
+# Start timer
+from timeit import default_timer as timer 
+
+from torchvision import transforms
+
+# Set up hyperparameters
+NUM_EPOCHS = 5
+BATCH_SIZE = 32
+HIDDEN_UNITS = 10
+LEARNING_RATE = 0.001
+
+# Setup directories
+train_dir = ""
+test_dir = ""
+
+# Setup target device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Create transforms
+data_transforms = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor()
+])
+
+# Create DataLoaders using data_setup.py
+train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
+    train_dir=train_dir,
+    test_dir=test_dir,
+    transform=data_transform,
+    batch_size=BATCH_SIZE
+)
+
+# Create model with help from model_builder.py
+model = model_builder.TinyVGG(
+    input_shape=3,
+    hidden_units=HIDDEN_UNITS,
+    output_shape=len(class_names)
+).to(device)
+
+# Set loss and optimizer
+loss_fn = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+start_time = timer()
+
+# Start training with help from engine.py
+engine.train(model=model,
+             train_dataloader=train_dataloader,
+             test_dataloader=test_dataloader,
+             loss_fn=loss_fn,
+             optimizer=optimizer,
+             epochs=NUM_EPOCHS,
+             device=device)
+
+# End timer and print total time
+end_time = timer()
+print(f"Total training time: {end_time-start_time:.3f} seconds.")
+
+# Save the model
+utils.save_model(model=model,
+                 target_dir="models",
+                 model_name="rubbish_classifier_tinyvgg_model.pth")
