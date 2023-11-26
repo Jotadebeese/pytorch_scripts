371343c615eee61513aa2d81c0d4e7ffcfbe8c3d
Juan <jotadebeese@gmail.com>
2023-11-26 11:09:22 +1100

    Added new fucntion to make a prediction and plot on a given image

--- modular_scripts/utils.py
+++ modular_scripts/utils.py
@@ -1,10 +1,13 @@
 """
 Contains various utility functions for PyTorch model training and saving
 """
-import torch, os, sys, requests, zipfile, gdown
+import torch, os, sys, requests, zipfile, gdown, torchvision
 from pathlib import Path
 from tqdm import tqdm
 from PIL import Image
+from typing import List, Tuple
+from torchvision import transforms
+import matplotlib.pyplot as plt
 
 def save_model(
     model: torch.nn.Module,
@@ -96,3 +99,64 @@ def get_data(zip_file_id: str):
     os.remove(str(data_path)+"/dataset.zip")
 
     return images_path
+
+def pred_and_plot_image(model: torch.nn.Module,
+                        image_path: str,
+                        class_names: List[str],
+                        image_size: Tuple[int, int] = (224, 224),
+                        transform: torchvision.transforms = None,
+                        device: torch.device=device):
+    """Make a prediction and plot an image using a given model.
+    
+    Args:
+        mode: Model to make the prediction
+        image_path: Path of the image on which the prediction will occur
+        class_names: Names of the classess to predict
+        transform: Transforms to apply on the image to predict. Good practices to use same as the used on the pretrained model.
+        device: Target device
+    
+    Example of use:
+        pred_and_plot_image(model=model, 
+                            image_path=image_path,
+                            class_names=class_names,
+                            # transform=weights.transforms(), # optionally pass in a specified transform from our pretrained model weights
+                            image_size=(224, 224))
+    """
+    # Open Image
+    img = Image.open(image_path)
+
+    # Create transformation for image (if one doesn't exist)
+    if transform is not None:
+        image_transform = transform
+    else:
+        image_transform = transforms.Compose([
+            transforms.Resize(image_size),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
+
+        ])
+    
+    ### Predict on image ### 
+    # Make sure mode is on the target device
+    model.to(device)
+
+    # Turn on model evaluation mode and inference mode
+    model.eval()
+    with torch.inference_mode():
+        # Transform and add an extra dimesion to image (model requires samples in [batch_size, color_channels, height, width])
+        transformed_image = image_transform(img).unsqueeze(dim=0)
+        # Make prediction on image with extra dimension and send it ot the target device
+        target_image_pred = model(transformed_image.to(device))
+    
+    # Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+
+    # Convert prediction probabilities -> prediction labels
+    target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
+
+    # Plot image with predicted label and probability
+    plt.figure()
+    plt.imshow(img)
+    plt.title(f"Pred: {class_names[target_image_pred_label]} | Prob: {target_image_pred_probs.max():.3f}")
+    plt.axis(False)
